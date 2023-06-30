eb73fd447dadd2eb64ba83294cf0a8e8a34930a3
ogNG4 <m.podobinski33@gmail.com>
2023-06-30 12:39:54 +0200

    update: 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="flex-col h-[100vh] flex items-center justify-center ">
       <h1 className="text-[12rem]">404</h1>
 
       <Link href="/">
-        <span>Back</span>
+        <span className="bg-amber-300 hover:bg-amber-400 px-2 py-1 rounded-md shadow-md text-white text-2xl font-bold ">
+          Back
+        </span>
       </Link>
     </div>
   );
